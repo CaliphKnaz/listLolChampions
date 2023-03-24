6562da7d65f4aea036e60a828b215c5a3401df4e
Tomi <48963553+CaliphKnaz@users.noreply.github.com>
2023-03-24 10:31:23 +0000

    formatting code

--- src/index.js
+++ src/index.js
@@ -11,39 +11,23 @@ const champAttack = document.getElementById("Attack")
 const champDefense = document.getElementById("Defense")
 const champMagic = document.getElementById("Magic")
 
-function champArray() {
-    fetch('http://ddragon.leagueoflegends.com/cdn/13.5.1/data/en_US/champion.json')
-        .then(res => res.json())
-        .then(data => {
-            try {
-                let champs = data.data
-                let champList1 = []
-                for (let char in champs) {
-                    champList1.push(char)
-                }
-                return champList1
-            } catch (error) {
-                console.log(error)
-            }
-        })
 
-}
 
 
 function fillDropdown() {
     const datalist = document.getElementById("champList") // assign the datalist variable to the elementid ChampList
 
     // fetch list of champions from API and retrieve the character names
-    // create and append each character name to the datalist variable and consequently the champlist element
     fetch('http://ddragon.leagueoflegends.com/cdn/13.5.1/data/en_US/champion.json')
         .then(res => res.json())
         .then(data => {
             try {
                 let champs = data.data
                 let champList1 = []
+                // create and append each character name to the datalist variable and consequently the champlist element
 
                 for (let char in champs) {
-                    champList1.push(char)
+                    champList1.push(char) // add champ names to a varaible to verify user input against
 
                     var option = document.createElement('option')
                     option.value = (champs[char].id)
@@ -76,7 +60,7 @@ function userValue(champList1) {
             displayINFO(champion)
             displayImage(champion)
 
-        } else {
+        } else { //load invalid input response
             champTitle.textContent = 'Lost Poros'
             champBlurb.textContent = 'Please input a valid champion'
             champImage.src = "https://nexus.leagueoflegends.com/wp-content/uploads/2018/11/poros_banner-1_slno1owbdsxulmdvqomp.jpg"
